88074e597bd0a1d8a1d2e28a73db3b55ccc4960b
Tyler Walker <tyler.j.walker@maine.edu>
2024-02-07 20:55:55 -0500

    feat(player): Make player use state This toggles the play button's text

--- lib/src/play_bar.dart
+++ lib/src/play_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'track.dart';
+import 'package:audioplayers/audioplayers.dart';
+// import 'track.dart';
 
 class PlayBar extends StatefulWidget {
   const PlayBar({super.key});
@@ -9,12 +10,64 @@ class PlayBar extends StatefulWidget {
 }
 
 class _PlayBarState extends State<PlayBar> {
-  bool isPlaying = false;
-  Widget build(context) {
-    if (playing) {
-      return Text("Playing!");
+  late bool playing; // = false;
+  late bool songSelected; //= false;
+  final player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    playing = false;
+    songSelected = false;
+  }
+
+  void play() {
+    AssetSource track = AssetSource("../assets/roll.mp3");
+
+    print(track.toString());
+    if (!playing) {
+      if (!songSelected) {
+        player.play(track);
+        setState(() {
+          songSelected = true;
+        });
+      }
+      player.resume();
+      setState(() {
+        playing = true;
+      });
     } else {
-      return Text("Stopped!");
+      //player.stop();
+      player.pause();
+      setState(() {
+        playing = false;
+      });
     }
+    print(playing);
+  }
+
+  // bool isPlaying = false;
+  // Widget build(context) {
+  //   return Column(
+  //     children: <Widget>[
+  //       if (playing) {
+  //         Text("Playing!");
+  //       } else {
+  //         Text("Stopped!");
+  //       }
+  //     ]
+  //   );
+  // }
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        if (playing) Text("Playing!") else Text("Stopped!"),
+        ElevatedButton.icon(
+          icon: Icon(Icons.play_arrow),
+          label: Text(playing ? "Play!" : "Stop!"),
+          onPressed: play,
+        ),
+      ],
+    );
   }
 }
