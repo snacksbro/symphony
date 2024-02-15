d8ed4c090a884103fbf4a18cdc6887a1f3f63875
Tyler Walker <tyler.j.walker@maine.edu>
2024-02-14 20:34:43 -0500

    feat(parser): Second pass at implementing it This one doesn't require the tags to be in any specific order

--- lib/src/parse_metadata.dart
+++ lib/src/parse_metadata.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:typed_data';
 import 'dart:convert';
@@ -48,20 +49,61 @@ class MetaObject {
     // Decoding it
     String decoded_header = latin1.decode(header);
 
-    // Parse out the values
-    List<String> album_arr = decoded_header.split("TALB");
-    List<String> artist_arr = album_arr[1].split("TPE1");
-    String song_album = artist_arr[0];
-    List<String> title_arr = artist_arr[1].split("TIT2");
-    String song_artist = title_arr[0];
-    List<String> newline_arr = title_arr[1].split("TSSE");
-    String song_title = newline_arr[0];
+    // Second approach
+    Map metaInfo = {
+      'album': {
+        // I misinterpreted what lastIndexOf does apparently
+        'start': decoded_header.lastIndexOf("TALB") + 4,
+        'end': decoded_header.lastIndexOf("TALB"),
+        'value': ""
+      },
+      'artist': {
+        'start': decoded_header.lastIndexOf("TPE1") + 4,
+        'end': decoded_header.lastIndexOf("TPE1"),
+        'value': ""
+      },
+      'title': {
+        'start': decoded_header.lastIndexOf("TIT2") + 4,
+        'end': decoded_header.lastIndexOf("TIT2"),
+        'value': ""
+      },
+      'settings': {
+        'start': decoded_header.lastIndexOf("TSSE") + 4,
+        'end': decoded_header.lastIndexOf("TSSE"),
+        'value': ""
+      }
+    };
 
-    // Printing for good measure
-    // print("TITLE IS " + song_title);
-    // print("ALBUM IS " + song_album);
-    // print("ARTIST IS " + song_artist);
-    // Preceeds ^C for chars? May be how to fix the title
+    // Grab all the start positions and sort ascending
+    List<int> startPositions = [];
+    metaInfo.forEach((key, value) {
+      startPositions.add(value['start']);
+      startPositions.sort();
+    });
+
+    // Figuring out where each end is
+    metaInfo.forEach((key, value) {
+      for (int i = 0; i < startPositions.length; i++) {
+        if (startPositions[i] > value['start']) {
+          value["end"] = startPositions[i] - 4;
+          print("${key} set to ${value['end']}");
+          break;
+        } else {
+          print("FAILURE ${startPositions[i]} !> ${value['start']}");
+        }
+      }
+    });
+
+    // Write to map and print
+    metaInfo.forEach((key, value) {
+      value['value'] = decoded_header.substring(value['start'], value['end']);
+      print("${key} ${value['start']}:${value['end']}");
+    });
+
+    // TODO: Remove these variables
+    String song_artist = metaInfo['artist']['value'];
+    String song_album = metaInfo['album']['value'];
+    String song_title = metaInfo['title']['value'];
     return [song_artist, song_album, song_title];
   }
 
